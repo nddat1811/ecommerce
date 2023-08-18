b64ee8bc2d77588ad930c27f0330f26f6ce5e4eb
nddat1811 <63857350+nddat1811@users.noreply.github.com>
2023-08-18 15:08:40 +0700

    success order page

--- frontend/pages/api/stripe.js
+++ frontend/pages/api/stripe.js
@@ -37,7 +37,7 @@ export default async function handler(req, res) {
           };
         }),
         //Bring people to success or failed page
-        success_url: `${req.headers.origin}/success`,
+        success_url: `${req.headers.origin}/success?&session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${req.headers.origin}/canceled`,
       });
       res.status(200).json(session);
